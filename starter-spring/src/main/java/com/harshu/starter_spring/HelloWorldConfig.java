8e1eee517fa6cd69e5f7287349432522198a6aa1
Harshith R <harshijeevi06022004@gmail.com>
2025-01-10 12:38:54 +0530

    primary and qualifier in beans

--- starter-spring/src/main/java/com/harshu/starter_spring/HelloWorldConfig.java
+++ starter-spring/src/main/java/com/harshu/starter_spring/HelloWorldConfig.java
@@ -2,8 +2,12 @@ package com.harshu.starter_spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 record Person(String name,int age) {};
 record Adress(String firstLine,String city) {};
+record Temp(String firstLine,int age) {};
+//to prove the ambuiguity
+record Exception(String name,int age) {};
 @Configuration
 public class HelloWorldConfig {
 	@Bean
@@ -14,14 +18,37 @@ public class HelloWorldConfig {
 	public int age() {
 		return 15;
 	}
+	//how to use the existing beans in the new bean
+	//method 1 passing it as parameters
 	@Bean
 	public Person person(String name,int age) {
 		var person=new Person(name,age);
 		return person;
 	}
-	@Bean
-	public Adress address(String firstLine,String city) {
+	//to use custom bean names in java
+	@Bean(name="adressline")
+	public Adress adress(String firstLine,String city) {
 		var address=new Adress("Bannerghatta","Bangalore");
 		return address;
 	}
+	//method 2 calling it in function
+		@Bean
+		public Temp Aname() {
+		   return new Temp(name(),age());
+		}
+	@Bean
+	public Person person1(String name,int age) {
+			var person=new Person(name,age);
+			return person;
+	}	
+	@Bean
+	@Primary
+	public Exception ex(String name,int age) {
+		return new Exception(name,age);
+	}
+	@Bean
+	public Exception ex() {
+		return new Exception(name(),age());
+	}
+		//if we have same bean name
 }
